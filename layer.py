d3e9c70d10a97b0b59c06a54a7af01a89996a82d
Kenny Tang <kenny.t4ng@gmail.com>
2025-08-08 15:54:15 +0700

    Add dense layer

---
+++ layer.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class DenseLayer:
+    # Layer initialization
+    def __init__(self, n_inputs, n_outputs):
+        self.weights = np.random.randn(n_inputs, n_outputs) * 0.01
+        self.biases = np.zeros((1, n_outputs)) 
+
+    # forward pass 
+    def forward(self, inputs):
+        ''' 
+        Linear Transformation 
+        Z = XW + b
+
+        X : inputs (n_samples, n_features)
+        W : weights (n_features, n_outputs)
+        b : biases (1 , n_outputs)
+        '''
+        
+        self.output = np.dot(inputs, self.weights) + self.biases
